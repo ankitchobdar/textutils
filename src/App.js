03a97a78dbc080b720084d63ba68e211dcd282f3
ankitchobdar <ankitchobdar@gmail.com>
2022-05-05 13:01:27 +0530

    react-router addition

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); //Dark mode state
@@ -24,21 +29,25 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert('Dark mode has been enabled', 'success');
-    } else {
+      } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert('Light mode has been enabled', 'success');
-    }      
+      }      
   };
 
   return (
     <>
-      <Navbar title="TextUtils" about="About us" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
-      <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}/> 
-        {/* <About/> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" about="About us" mode={mode} toggleMode={toggleMode}/>
+        <Alert alert={alert}/>
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About/>} />
+            <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}/>} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
